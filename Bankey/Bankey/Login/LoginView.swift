46c6cdc9966ce9e770da502498cb46da3ea40512
Maybelline <maybelline.rivas@gmail.com>
2023-08-25 16:43:15 -0700

    Add login screen

---
+++ Bankey/Bankey/Login/LoginView.swift
@@ -0,0 +1,94 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Maybelline Rivas on 8/24/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    let stackView = UIStackView()
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let dividerView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style() // styling
+        layout() // controlling
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    override var intrinsicContentSize: CGSize {
+//        return CGSize(width: 200, height: 200) // Is the default size the views want to size themselves in the view. Since the views inside the stackView (TextVies) already have an intrinsic content size, this is no longer needed
+//    }
+}
+
+extension LoginView { //extension is a way to pull out chunks of funtionality to group them logically
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false //Takes any view and makes it ready for autolayout
+        backgroundColor = .secondarySystemBackground
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.placeholder = "Username"
+        usernameTextField.delegate = self
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.delegate = self
+        
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .secondarySystemFill
+        
+        layer.cornerRadius = 5
+        clipsToBounds = true //this means clip or exclude any other backgroud images from that corner
+    }
+    
+    func layout() {
+        
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(dividerView)
+        stackView.addArrangedSubview(passwordTextField)
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
+        ])
+        
+        dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+       return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+}

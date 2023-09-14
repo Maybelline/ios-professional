19edbef2c41678e756ebba5ac0dc4688fa5aaed0
Maybelline <maybelline.rivas@gmail.com>
2023-09-14 15:57:13 -0700

    feat: Add networking to AccountSummary

--- Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class AccountSummaryViewController: UIViewController {
     
-    var accounts:[AccountSummaryCell.ViewModel] = []
+    //Requesr models
+    var profile: Profile?
+    var accounts: [Account] = []
+    
+    //View Models
+    var headerViewModel = AccSummaryHeaderView.ViewModel(welcomeMessage: "Welcome", name: "", date: Date())
+    var accountCellViewModels:[AccountSummaryCell.ViewModel] = []
+    
     var tableView = UITableView()
+    
+    var headerView = AccSummaryHeaderView(frame: .zero)
     
     lazy var logoutBarButtonItem: UIBarButtonItem = {
         let barButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logoutTapped))
@@ -22,7 +31,8 @@ class AccountSummaryViewController: UIViewController {
         super.viewDidLoad()
         setup()
         setupNavigationBar()
-        fetchData()
+        //fetchAccounts()
+        fetchDataAndLoadViews()
     }
     
     func setupNavigationBar(){
@@ -57,28 +67,27 @@ extension AccountSummaryViewController {
     }
     
     private func setupTableHeaderView(){
-        let header = AccSummaryHeaderView(frame: .zero)
         
-        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize) // this asks the header to lay iself out in the smallest form it can
+        var size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize) // this asks the header to lay iself out in the smallest form it can
         size.width = UIScreen.main.bounds.width // this takes the width of whatever phone we have
-        header.frame.size = size // this takes the intrinsic size set above to 144
-        tableView.tableHeaderView = header
+        headerView.frame.size = size // this takes the intrinsic size set above to 144
+        tableView.tableHeaderView = headerView
     }
 }
 
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard !accounts.isEmpty else { return UITableViewCell() }
-                
-                let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
-                let account = accounts[indexPath.row]
-                cell.configure(with: account)
-                
-                return cell
+        guard !accountCellViewModels.isEmpty else { return UITableViewCell() }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
+        let account = accountCellViewModels[indexPath.row]
+        cell.configure(with: account)
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return accounts.count
+        return accountCellViewModels.count
     }
 }
 
@@ -89,31 +98,31 @@ extension AccountSummaryViewController: UITableViewDelegate {
 }
 
 extension AccountSummaryViewController {
-    private func fetchData() {
+    private func fetchAccounts() {
         let savings = AccountSummaryCell.ViewModel(accountType: .Banking,
-                                                            accountName: "Basic Savings",
-                                                        balance: 929466.23)
+                                                   accountName: "Basic Savings",
+                                                   balance: 929466.23)
         let chequing = AccountSummaryCell.ViewModel(accountType: .Banking,
                                                     accountName: "No-Fee All-In Chequing",
                                                     balance: 17562.44)
         let visa = AccountSummaryCell.ViewModel(accountType: .CreditCard,
-                                                       accountName: "Visa Avion Card",
-                                                       balance: 412.83)
+                                                accountName: "Visa Avion Card",
+                                                balance: 412.83)
         let masterCard = AccountSummaryCell.ViewModel(accountType: .CreditCard,
-                                                       accountName: "Student Mastercard",
-                                                       balance: 50.83)
+                                                      accountName: "Student Mastercard",
+                                                      balance: 50.83)
         let investment1 = AccountSummaryCell.ViewModel(accountType: .Investment,
                                                        accountName: "Tax-Free Saver",
                                                        balance: 2000.00)
         let investment2 = AccountSummaryCell.ViewModel(accountType: .Investment,
                                                        accountName: "Growth Fund",
                                                        balance: 15000.00)
-        accounts.append(savings)
-        accounts.append(chequing)
-        accounts.append(visa)
-        accounts.append(masterCard)
-        accounts.append(investment1)
-        accounts.append(investment2)
+        accountCellViewModels.append(savings)
+        accountCellViewModels.append(chequing)
+        accountCellViewModels.append(visa)
+        accountCellViewModels.append(masterCard)
+        accountCellViewModels.append(investment1)
+        accountCellViewModels.append(investment2)
     }
 }
 
@@ -121,5 +130,48 @@ extension AccountSummaryViewController {
 extension AccountSummaryViewController {
     @objc func logoutTapped(sender: UIButton) {
         NotificationCenter.default.post(name: .logout, object: nil)
+    }
+}
+
+// MARK: - Networking
+extension AccountSummaryViewController {
+    private func fetchDataAndLoadViews() {
+        
+        fetchProfile(forUserId: "1") { result in
+            switch result {
+            case .success(let profile):
+                self.profile = profile
+                self.configureTableHeaderView(with: profile)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        fetchAccounts(forUserId: "1") { result in
+            switch result {
+            case .success(let accounts):
+                self.accounts = accounts
+                self.configureTableCells(with: accounts)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func configureTableHeaderView(with profile: Profile) {
+        let vm = AccSummaryHeaderView.ViewModel(welcomeMessage: "Good morning,",
+                                                name: profile.firstName,
+                                                date: Date())
+        headerView.configure(viewModel: vm)
+    }
+    
+    private func configureTableCells(with accounts: [Account]) {
+        accountCellViewModels = accounts.map {
+            AccountSummaryCell.ViewModel(accountType: $0.type,
+                                         accountName: $0.name,
+                                         balance: $0.amount)
+        }
     }
 }
--- Bankey/Bankey/AccountSummary/Header/AccSummaryHeaderView.swift
+++ Bankey/Bankey/AccountSummary/Header/AccSummaryHeaderView.swift
@@ -11,7 +11,24 @@ class AccSummaryHeaderView: UIView{
     
     @IBOutlet var contentView: UIView!
     
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
     let shakeyBellView = ShakeyBellView()
+    
+    struct ViewModel{
+        let welcomeMessage: String
+        let name: String
+        let date: Date
+        
+        var dateFormatted: String{
+            return date.monthDayYearString
+        }
+        
+    }
     
     override init(frame: CGRect) {
           super.init(frame: frame)
@@ -52,4 +69,9 @@ class AccSummaryHeaderView: UIView{
            ])
        }
     
+    func configure(viewModel: ViewModel){
+        welcomeLabel.text = viewModel.welcomeMessage
+        nameLabel.text = viewModel.name
+        dateLabel.text = viewModel.dateFormatted
+    }
 }

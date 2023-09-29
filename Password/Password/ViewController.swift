7f8c42cd816e5e2d3e14b62c26fc8e6df51675b1
Maybelline <maybelline.rivas@gmail.com>
2023-09-29 13:38:43 -0700

    feat: add password criteria view

--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController{
     
     let newPasswordTextField = PasswordTextField(placeHolderText: "New password")
     let stackView = UIStackView()
+    let criteriaView = PasswordCriteriaView(text: "uppercase letter (A-Z)")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +26,18 @@ extension ViewController {
         stackView.axis = .vertical
         stackView.spacing = 20
         newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+        criteriaView.translatesAutoresizingMaskIntoConstraints = false
         
     }
     
     func layout(){
         view.addSubview(stackView)
-        stackView.addArrangedSubview(newPasswordTextField)
+        //stackView.addArrangedSubview(newPasswordTextField)
+        stackView.addArrangedSubview(criteriaView)
         
         NSLayoutConstraint.activate([stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
                                      view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
-                                     newPasswordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                                     stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         
         ]
         )
